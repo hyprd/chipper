b9431c2d355549947b7278a7eb568f7d71d9bbab
hyprd <luuzeish@gmail.com>
2021-06-14 17:44:47 +1200

    gitignore, fontset loader added

---
+++ src/chip8.h
@@ -0,0 +1,18 @@
+#include <stdint.h>
+
+class Chip8 {
+    public: 
+        Chip8();
+        ~Chip8();
+        uint8_t display[2048];
+        void Initialise();
+        void LoadFontsetToMemory();
+    private:
+        uint8_t memory[4096];
+        uint16_t pc;
+        uint16_t indexRegister; // aka "I"
+        uint16_t stack;
+        uint8_t delayTimer;
+        uint8_t soundTimer;
+        uint8_t variableRegister[16]; // aka "V"
+};
